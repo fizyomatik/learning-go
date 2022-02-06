1029faceaa8a84383b723898710b2c8c5bb582a7
mehmet <baykara.e41@gmail.com>
2022-02-06 13:54:15 +0100

    Slice

--- append1.go
+++ append1.go
@@ -4,12 +4,26 @@ import "fmt"
 
 func main() {
 	items := []string{
-		"hello", "dog", "cat", "cow", "fish",
-		"bird", "leo", "moon",
+		"hello", "dog", "cat", "cow",
+		"fish", "bird", "leo", "moon",
+		"me", "you", "jo", "water",
+		"me",
 	}
 
-	top3 := items[:3]
-	fmt.Print("Items top3:\n ", top3)
+	const pageSize = 4
+	l := len(items)
+
+	for from := 0; from < l; from += pageSize {
+		to := from + pageSize
+		if to > l {
+			to = l
+		}
+
+		currentPage := items[from:to]
+		head := fmt.Sprintf("Page #%d\n", (from/pageSize)+1)
+		fmt.Println()
+		fmt.Print(head, currentPage)
+	}
 	fmt.Println()
 
 }
